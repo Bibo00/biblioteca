d311d04c45f3762616e63ab78a8659f13f76b69a
Bibo00 <bibinileonardo2@gmail.com>
2025-04-04 13:13:11 +0200

    cambio, da ajax a fetch API

--- public/assets/js/ajax.js
+++ public/assets/js/ajax.js
@@ -1,16 +1,55 @@
 function trovaLibri(query, callback) {
-    // Creiamo la richiesta AJAX
-    //console.log(query);
     query = query.replace(/ /g, "-");
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `home/ricerca/${query}`, true);  //nell url GET inserisce il valore di quanto inserito nella navbar
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            // Se la risposta è corretta (status 200), processa i dati JSON
-            const books = JSON.parse(xhr.responseText);
-            const img = document.createElement('img');
-            callback(books);
-        }
-    };
-    xhr.send();
+    fetch(`home/ricerca/${query}`)
+        .then(response => {
+            if(!response.ok) throw new Error(`Errore ${response.status}`);
+            return response.json();
+        })
+        .then(callback)
+        .catch(onError);
 }
+
+
+function findAll(callback, onError = console.error) {
+    fetch('home/findAll')
+        .then(response => {
+            if (!response.ok) throw new Error(`Errore ${response.status}`);
+            return response.json();
+        })
+        .then(callback)
+        .catch(onError);
+}
+
+function renderBooks(books) {
+    const main = document.getElementById('main-content');
+    if (!main || books.length === 0) return;
+
+    let bookRow = document.createElement('div');
+    bookRow.classList.add('book-row');
+
+    books.forEach((book, i) => {
+        if (i % 6 === 0 && i !== 0) {
+            main.appendChild(bookRow);
+            bookRow = document.createElement('div');
+            bookRow.classList.add('book-row');
+        }
+
+        const bookItem = document.createElement('div');
+        bookItem.classList.add('book-item');
+        bookItem.id = book.IdL;
+        bookItem.innerHTML = `
+            <img src="data:image/png;base64,${book.Copertina}" alt="${book.Titolo}">
+            <p>${book.Titolo} (${book.Anno}) - ${book.Autore}</p>
+        `;
+
+        bookRow.appendChild(bookItem);
+    });
+
+    if (bookRow.children.length > 0) {
+        main.appendChild(bookRow);
+    }
+}
+
+window.addEventListener('load', () => {
+    findAll(renderBooks);
+});
