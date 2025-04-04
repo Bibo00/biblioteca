ae250e0d593ac129cb5178b334768d17580de56f
Bibo00 <bibinileonardo2@gmail.com>
2025-04-04 13:16:32 +0200

    aggiunta funzione findAll a controller Home, minor changes a index

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -17,7 +17,18 @@ class Home extends Controller
         //print_r($ris);
         //toBase64($ris);
         $jsonris = json_encode($ris);
-        //print_r($jsonris);
+        print_r($jsonris);
+        //$this->view('home');
+        return $jsonris;
+    }
+
+    public function findAll(){
+        $user = new User;
+        $ris = $user->findAll();
+        //print_r($ris);
+        //toBase64($ris);
+        $jsonris = json_encode($ris);
+        print_r($jsonris);
         //$this->view('home');
         return $jsonris;
     }
--- public/index.php
+++ public/index.php
@@ -1,13 +1,4 @@
 <?php
-
-
-/*function show($s)
-{
-    echo '<pre>';
-    print_r($s);
-    echo '</pre>';
-}*/
-
 session_start();
 
 require '../app/core/init.php';
